074a6361d5ce00e9833cb1d3eeddcf2523129287
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-02-13 00:28:00 +0100

    greeting with React.js and babel (and a lot of other not necessary things?)

--- webapp/views.py
+++ webapp/views.py
@@ -8,3 +8,7 @@ webapp = Blueprint('webapp', __name__,
 @webapp.route('/')
 def index():
     return render_template(os.path.join('pages', 'hello.html'), name='Rreli')
+
+@webapp.route('/reaction')
+def reaction():
+    return render_template('reaction.html')
