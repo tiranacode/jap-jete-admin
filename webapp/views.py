86cfdbc12e88cb4b19a535436576a8725caf1308
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-02-13 01:36:59 +0100

    moved the JSX content to a static jsx/server-call.js file

--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, Flask
 import os
 
 
 webapp = Blueprint('webapp', __name__,
-                        template_folder=os.path.join('static', 'templates'))
+                    static_folder='static',
+                    static_url_path='/webapp/static',
+                    template_folder=os.path.join('static', 'templates'))
 
 @webapp.route('/')
 def index():
