33592274d3ab4aa2483ce0ff860b20922956acc1
Tomas Pllaha <tomas@cliqz.com>
2016-02-23 01:29:36 +0100

    Fix require_login, improve defence against timing attack

--- src/api/decorators.py
+++ src/api/decorators.py
@@ -1,20 +1,23 @@
 from db import *
-from flask import request
 from utils import ApiResponse
 import config
 import time, datetime
 import utils
 import random
 
+from functools import wraps
+from flask import g, request
+
 def require_login(handler):
-    def safe_handler():
+    @wraps(handler)
+    def safe_handler(*args, **kwargs):
         session_token = request.args.get('session_token', '')
         user_id = request.args.get('user_id', 0)
-        user = session.query(User).filter_by(user_id=user_id, session_token=session_token).first()
+        user = session.query(User).filter_by(user_id=user_id).first()
         sleep_amount = random.random()/5
-        if user and user.session_token == session_token and \
+        if user and utils.str_equal(str(user.session_token), str(session_token)) and \
             utils.to_timestamp(user.session_token_expires_at) > time.time():
-            return handler()
+            return handler(*args, **kwargs)
         else:
             time.sleep(sleep_amount)
             return ApiResponse(config.ACCESS_DENIED_MSG, status='403')
