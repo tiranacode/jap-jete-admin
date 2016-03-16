575b8a8c00e04510bb61984c014925b6e0ac4698
ani12321 <ani_xhaja@hotmail.com>
2016-03-17 00:15:55 +0100

    changed routing to hashHistory

--- src/webapp/index.js
+++ src/webapp/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, browserHistory } from 'react-router';
+import { Router, Route, Link, hashHistory } from 'react-router';
 import App from './app/App';
 import Donators from './app/components/Donators'
 
 //Render routes
 ReactDOM.render((
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
         <Route path="/" component={App}>
             <Route path="donators" component={Donators}/>
         </Route>
